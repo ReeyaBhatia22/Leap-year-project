402d74517911563fa78a2891b6708fe32361b170
ReeyaBhatia22 <reeyabhatia22@gmail.com>
2024-10-15 20:18:25 +0530

    After class project

---
+++ Project.java
@@ -0,0 +1,10 @@
+public class Project
+{
+    public static void main(String[]args)
+    {
+        int a = 2024;
+        System.out.println("The given year is:"+ a);
+        Boolean result= (a%4==0)?true:false;
+        System.out.println("is "+ a+ " a leap year:"+ result);
+}
+}
